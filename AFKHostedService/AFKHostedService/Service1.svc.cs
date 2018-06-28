12bf01f267e07afadf6e255a8b86fbe6fda42bdf
Duncan Campbell <cmpdun001@myuct.ac.za>
2018-06-28 11:32:20 +0200

    API Data Contracts

--- AFKHostedService/AFKHostedService/Service1.svc.cs
+++ AFKHostedService/AFKHostedService/Service1.svc.cs
@@ -12,10 +12,59 @@ namespace AFKHostedService
     // NOTE: In order to launch WCF Test Client for testing this service, please select AFKHostedService.svc or AFKHostedService.svc.cs at the Solution Explorer and start debugging.
     public class AFKHostedService : IService
     {
-        public string GetData(int value)
+        public List<DataBaseEntry> GetEntries()
         {
-            return string.Format("You entered: {0}", value);
+            //Connect to database
+            //Get all entries
+
+            //Testing
+            DataBaseEntry x = new DataBaseEntry("EventType", "UserID", "DeviceID", new DateTime(2018, 6, 27, 15, 30, 50), true, new TimeSpan(15, 45, 50));
+            List<DataBaseEntry> test = new List<DataBaseEntry>();
+            test.Add(x);
+            return test;
         }
 
+        public List<DataBaseEntry> GetEntriesOfUser(string UserID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataBaseEntry> GetEntriesBetween(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataBaseEntry> GetEntriesBetweenForUser(string UserID, DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TimeSpan RemainingTime(DataBaseEntry entry)
+        {
+            TimeSpan remaining = entry.ETA - (DateTime.Now.Subtract(entry.TimeOfEvent));
+            if (remaining < new TimeSpan(0, 0, 0))
+            {
+                return new TimeSpan(0, 0, 0);
+            }
+            else
+            {
+                return remaining;
+            }
+        }
+
+        public List<Employee> GetEntriesForAlice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EntryOutput(DataBaseEntry str)
+        {
+            return str.EventType + ", " + str.UserID + ", " + str.DeviceID + ", " + str.AutomaticLock + ", " + str.ETA.ToString() + ", " + str.TimeOfEvent.ToString();
+        }
+
+        public void AddEntry(DataBaseEntry entry)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
