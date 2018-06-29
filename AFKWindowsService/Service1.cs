1f74a3099734e6cbf1415a87bb3a36678bf0e1ec
BensonJ <BensonJ@INTERNBOX4>
2018-06-29 15:55:40 +0200

    Implemented SID-based identication

--- AFKWindowsService/Service1.cs
+++ AFKWindowsService/Service1.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.DirectoryServices;
 using System.Linq;
+using System.Security.Principal;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,19 +25,30 @@ namespace AFKWindowsService
             CanHandleSessionChangeEvent = true;
             InitializeComponent();
 
-            deviceID = System.Environment.MachineName;
-            userID = System.Environment.UserName;
+            deviceID = new SecurityIdentifier((byte[])new DirectoryEntry(string.Format("WinNT://{0},Computer", Environment.MachineName)).Children.Cast<DirectoryEntry>().First().InvokeGet("objectSID"), 0).AccountDomainSid.ToString();
+            userID = WindowsIdentity.GetCurrent().User.AccountDomainSid.ToString();
 
+            using (ServiceClient c = new ServiceClient())
+            {
+                Device device = new Device();
+                device.DeviceID = deviceID;
+                device.DeviceName = Environment.MachineName;
+                device.UserID = userID;
+                device.UserName = Environment.UserName;
+                device.VM = false;
+
+                c.AddDevice(device);
+            }
 
 
 
             eL = new EventLog();
             if (!System.Diagnostics.EventLog.SourceExists("MySource"))
             {
-                System.Diagnostics.EventLog.CreateEventSource("MySource", "AFKServiceLog");
+                System.Diagnostics.EventLog.CreateEventSource("MySource", "MyNewLog");
             }
             eL.Source = "MySource";
-            eL.Log = "AFKServiceLog";
+            eL.Log = "MyNewLog";
 
 
         }
@@ -58,7 +71,7 @@ namespace AFKWindowsService
                 dBE.RemoteAccess = System.Windows.Forms.SystemInformation.TerminalServerSession;
 
                 await c.AddEntryAsync(dBE);
-                eL.WriteEntry(c.GetAllEntries().First().EventType);
+                //eL.WriteEntry(c.GetAllEntries().First().EventType);
             }
         }
 
--- AppletTesting/AppletTesting/Form1.Designer.cs
+++ AppletTesting/AppletTesting/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.btnETA1 = new System.Windows.Forms.Button();
             this.btnETA2 = new System.Windows.Forms.Button();
             this.btnETA3 = new System.Windows.Forms.Button();
+            this.btnAddDevice = new System.Windows.Forms.Button();
+            this.chkVM = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // btnETA1
@@ -63,17 +65,40 @@
             this.btnETA3.UseVisualStyleBackColor = true;
             this.btnETA3.Click += new System.EventHandler(this.btnETA3_Click);
             // 
+            // btnAddDevice
+            // 
+            this.btnAddDevice.Location = new System.Drawing.Point(13, 43);
+            this.btnAddDevice.Name = "btnAddDevice";
+            this.btnAddDevice.Size = new System.Drawing.Size(75, 23);
+            this.btnAddDevice.TabIndex = 3;
+            this.btnAddDevice.Text = "Add Device";
+            this.btnAddDevice.UseVisualStyleBackColor = true;
+            this.btnAddDevice.Click += new System.EventHandler(this.btnAddDevice_Click);
+            // 
+            // chkVM
+            // 
+            this.chkVM.AutoSize = true;
+            this.chkVM.Location = new System.Drawing.Point(108, 47);
+            this.chkVM.Name = "chkVM";
+            this.chkVM.Size = new System.Drawing.Size(105, 17);
+            this.chkVM.TabIndex = 4;
+            this.chkVM.Text = "Virtual Machine?";
+            this.chkVM.UseVisualStyleBackColor = true;
+            // 
             // AFKApplet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(264, 47);
+            this.ClientSize = new System.Drawing.Size(264, 80);
+            this.Controls.Add(this.chkVM);
+            this.Controls.Add(this.btnAddDevice);
             this.Controls.Add(this.btnETA3);
             this.Controls.Add(this.btnETA2);
             this.Controls.Add(this.btnETA1);
             this.Name = "AFKApplet";
             this.Text = "AFK Applet";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +107,8 @@
         private System.Windows.Forms.Button btnETA1;
         private System.Windows.Forms.Button btnETA2;
         private System.Windows.Forms.Button btnETA3;
+        private System.Windows.Forms.Button btnAddDevice;
+        private System.Windows.Forms.CheckBox chkVM;
     }
 }
 
